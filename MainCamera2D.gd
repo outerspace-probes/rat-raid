a9b618b14b0c155a48dbc5fff312f781de7ebed5
outerspace-probes <hello@outerspace.pl>
2019-12-08 20:20:16 +0100

    player left-right movement

--- MainCamera2D.gd
+++ MainCamera2D.gd
@@ -3,9 +3,9 @@ extends Camera2D
 var playerNode
 
 func _ready():
-	playerNode = get_tree().get_root().find_node("PlayerRatArea2D", true, false)
+	playerNode = get_tree().get_root().find_node("PlayerRat", true, false)
 	self.position = playerNode.position
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	self.position = playerNode.position
+	self.position.y = playerNode.position.y
--- Player.gd
+++ Player.gd
@@ -1,11 +1,21 @@
 extends Area2D
 
-export var speedDefault = 60
+export var speedDefault = 240
+export var speedLeftRight = 280
+var speedCurrent = speedDefault
 
 func _ready():
 	pass # Replace with function body.
 
 func _process(delta):
 	
-	self.position.y -= speedDefault * delta
+	# moving up
+	self.position.y -= speedCurrent * delta
+	# moving left-right
+	if Input.is_action_pressed("MOVE_LEFT"):
+		self.position.x -= speedLeftRight * delta
+	if Input.is_action_pressed("MOVE_RIGHT"):
+		self.position.x += speedLeftRight * delta
 	
+func _input(event):
+	pass
