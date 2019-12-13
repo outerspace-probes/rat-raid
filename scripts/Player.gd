ca01b90944d4c1586f4cb8493aa05a57187a7413
outerspace-probes <hello@outerspace.pl>
2019-12-13 16:08:50 +0100

    player sprite & animations

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,6 +1,9 @@
 extends Area2D
 
 export var speedDefault = 360
+export var speedMin = 160
+export var speedMax = 460
+export var speedAccelerationBrake = 360
 export var speedLeftRight = 380
 
 var speedCurrent = speedDefault
@@ -9,25 +12,38 @@ var bridgeNode
 var routeNode
 var initPos
 var isOnBridge = false
+onready var AnimPlayer = get_node("RatAnimationPlayer")
 
 func _ready():
 	initPos = self.position
 	bridgeNode = get_tree().get_root().find_node("RouteBridgesTileMap", true, false)
 	routeNode = get_tree().get_root().find_node("RouteTileMap", true, false)
+	AnimPlayer.play("RatRunStraight")
 	
 func _process(delta):
 	
 	move(delta)
 			
 func _input(event):
-	
-	if event.is_action_pressed("FIRE") && event.echo == false:
+	# fire
+	if event.is_action_pressed("FIRE") && event.is_echo() == false:
 		createLaser(self.position)
+		
+	# left-right animations	
+	if event.is_action_pressed("MOVE_LEFT") && event.is_echo() == false:	
+		AnimPlayer.set_current_animation("RatRunLeft")
+	if event.is_action_released("MOVE_LEFT"):
+		AnimPlayer.set_current_animation("RatRunStraight")
+		
+	if event.is_action_pressed("MOVE_RIGHT") && event.is_echo() == false:	
+		AnimPlayer.set_current_animation("RatRunRight")
+	if event.is_action_released("MOVE_RIGHT"):
+		AnimPlayer.set_current_animation("RatRunStraight")
 
 func move(delta):
-	
 	# moving up
 	self.position.y -= speedCurrent * delta
+	
 	# moving left-right
 	if Input.is_action_pressed("MOVE_LEFT"):
 		self.position.x -= speedLeftRight * delta
